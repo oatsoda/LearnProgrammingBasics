fa2f9986254566a1d44aff52566ca0e1c277f437
Andrew Milne <andrew+github@milne.tel>
2020-05-24 14:23:02 +0100

    Basic Input/Output

--- 1_InputOutput.Example/Program.cs
+++ 1_InputOutput.Example/Program.cs
@@ -6,7 +6,24 @@ namespace _1_InputOutput.Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // (This is a comment - it isn't part of the program!)
+
+            // Here is an example of basic Input and Output of a Console application!
+            // Try running it and see the text output to the screen.  Then see
+            // that you can give the program some input and it outputs it for you.
+            
+            // We do this with Console.Write and Console.Read
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("This is some output! Hello :)"); // Output something!
+            Console.WriteLine();                                // Output a blank line
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Next, type some Input please, followed by the Enter key:"); // This is more Output - we want to prompt you to type some input next
+            var input = Console.ReadLine();                     // This reads the Input - the keys you type
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("You typed: " + input);           // This Outputs what you Input!
         }
     }
 }
