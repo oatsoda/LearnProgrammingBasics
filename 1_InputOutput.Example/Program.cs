74960394779e0f0f9bce989937e037008613fdf5
Andrew Milne <andrew+github@milne.tel>
2020-05-24 14:38:24 +0100

    Comments

--- 1_InputOutput.Example/Program.cs
+++ 1_InputOutput.Example/Program.cs
@@ -12,7 +12,7 @@ namespace _1_InputOutput.Example
             // Try running it and see the text output to the screen.  Then see
             // that you can give the program some input and it outputs it for you.
             
-            // We do this with Console.Write and Console.Read
+            // We do this with Console.WriteLine and Console.ReadLine
 
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("This is some output! Hello :)"); // Output something!
@@ -20,7 +20,9 @@ namespace _1_InputOutput.Example
 
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Next, type some Input please, followed by the Enter key:"); // This is more Output - we want to prompt you to type some input next
+            
             var input = Console.ReadLine();                     // This reads the Input - the keys you type
+            // ^ This is called a "variable" - we are using this  to "store" what you will type
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("You typed: " + input);           // This Outputs what you Input!
